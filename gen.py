20b1e5c9714ef2998b30ffb4f727064cbf0900e3
Usermode <scottjsh@naver.com>
2025-07-19 07:51:28 +0000

    Update

--- gen.py
+++ gen.py
@@ -14,7 +14,8 @@ except ModuleNotFoundError:
     os.system('pip install undetected-chromedriver')
     import undetected_chromedriver as uc
 
-def create_hm_account(username, email, password):
+
+def create_hm_account(username, email, password, firstname):
     options = uc.ChromeOptions()
     options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 
@@ -23,8 +24,9 @@ def create_hm_account(username, email, password):
     wait = WebDriverWait(driver, 100)
 
     print(Fore.LIGHTMAGENTA_EX + "Opening Browser..")
-    
+
     driver.get("https://www2.hm.com/ko_kr/register")
+
     while True:
         try:
             driver.find_element(By.ID, "email").send_keys(email)
@@ -32,49 +34,58 @@ def create_hm_account(username, email, password):
             driver.find_element(By.ID, "dateOfBirth-D").send_keys("1")
             driver.find_element(By.ID, "dateOfBirth-M").send_keys("1")
             driver.find_element(By.ID, "dateOfBirth-Y").send_keys("2000")
-			driver.find_element(By.ID, "termsConditions").click()
-			driver.find_element(By.ID, "hmCollectionStorage").click()
-			driver.find_element(By.ID, "overseasTransfer").click()
-            time.sleep(.8)
-            driver.find_element(By.XPATH, '//*[@id="__next"]/main/div/form/button[10]').click()
+            driver.find_element(By.ID, "termsConditions").click()
+            driver.find_element(By.ID, "hmCollectionStorage").click()
+            driver.find_element(By.ID, "overseasTransfer").click()
+            time.sleep(0.8)
+            driver.find_element(
+                By.XPATH, '//*[@id="__next"]/main/div/form/button[10]').click()
             break
-        except:
+        except Exception:
             pass
+
         try:
             driver.find_element(By.ID, "name").send_keys(firstname)
             break
-        except:
+        except Exception:
             pass
 
     time.sleep(1)
     print("Account created! Trying to get spotify code...")
 
     try:
-        # Bu kısmı değiştirdik
-        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/ul/li[2]/button/article/div[1]/span/img'))).click()
+        # Spotify 코드 가져오기
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="__next"]/main/div/ul/li[2]/button/article/div[1]/span/img'))).click()
         time.sleep(1)
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/div[3]/div/div[4]/div/a'))).click()
-        offer_code_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id="__next"]/main/div/div[3]/div/div[3]/label")))
-        offer_code = "https://www.spotify.com/kr-ko/ppt/hm/?code=" + offer_code_element.text.replace("할인 코드 ", "")
+        wait.until(EC.visibility_of_element_located(
+            (By.XPATH, '//*[@id="__next"]/main/div/div[3]/div/div[4]/div/a'))).click()
+        offer_code_element = wait.until(EC.visibility_of_element_located(
+            (By.XPATH, '//*[@id="__next"]/main/div/div[3]/div/div[3]/label')))
+        offer_code = "https://www.spotify.com/kr-ko/ppt/hm/?code=" + \
+            offer_code_element.text.replace("할인 코드 ", "")
         print(f"Offer : {offer_code}")
 
-        # Kodu dosyaya ekleyin
+        # 코드를 파일에 저장
         with open("codes.txt", "a") as file:
             file.write(offer_code + "\n")
-            
+
     except Exception as e:
         print("Error:", e)
     finally:
         driver.quit()
 
-number_of_codes_to_collect = 10  
+
+number_of_codes_to_collect = 10
+
 for _ in range(number_of_codes_to_collect):
     username = f"a{''.join(random.sample(string.ascii_lowercase + string.digits, 15))}"
     firstname = f"A{''.join(random.sample(string.ascii_lowercase + string.digits, 14))}"
     lastname = f"L{''.join(random.sample(string.ascii_lowercase + string.digits, 14))}"
     password = f"A{''.join(random.sample(string.ascii_lowercase + string.digits, 15))}&*"
     email = f"{firstname}.{lastname}@example.com"
-    
-    print(Fore.LIGHTMAGENTA_EX + f"Spotify Gen | ID: {username} PW: {password} E-Mail: {email}")
-    create_hm_account(username, email, password)
+
+    print(Fore.LIGHTMAGENTA_EX +
+          f"Spotify Gen | ID: {username} PW: {password} E-Mail: {email}")
+    create_hm_account(username, email, password, firstname)
     time.sleep(5)
